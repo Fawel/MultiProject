2283b537c18d2f13c7d388036fedb16dd89c443a
Fawel <uiasop@gmail.com>
2019-02-24 22:08:40 +0300

    Lecture 14: Set callbacks for OnComponentBeginOverlap and OnComponentEndOverlap for platformTrigger

--- Source/MultiProject1/PlatformTrigger.cpp
+++ Source/MultiProject1/PlatformTrigger.cpp
@@ -7,7 +7,7 @@
 // Sets default values
 APlatformTrigger::APlatformTrigger()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	TriggerObject = CreateDefaultSubobject<UBoxComponent>(FName("TriggerObject"));
@@ -17,13 +17,15 @@ APlatformTrigger::APlatformTrigger()
 	}
 
 	RootComponent = TriggerObject;
+	TriggerObject->OnComponentBeginOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapBegin);
+	TriggerObject->OnComponentEndOverlap.AddDynamic(this, &APlatformTrigger::OnOverlapEnd);
 }
 
 // Called when the game starts or when spawned
 void APlatformTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -31,5 +33,17 @@ void APlatformTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APlatformTrigger::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (HasAuthority())
+		UE_LOG(LogTemp, Warning, TEXT("Begin Overlap"));
+}
+
+void APlatformTrigger::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	if (HasAuthority())
+		UE_LOG(LogTemp, Warning, TEXT("End Overlap"));
 }
 
